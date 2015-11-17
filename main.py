7bff3092435bd6bf75ee275a5a017c1401548606
Justin Leo Ye <justinleoye@gmail.com>
2015-11-17 11:54:08 +0800

    print min_sup

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from issue_routines import get_issue_routines
 
 routines = get_issue_routines()
 min_sup = int(len(routines) * 0.1)                             #最小支持度计数
+print '#'*40
+print 'min_sup:', min_sup
+print '#'*40
 headerTable = {}        #头结点表，用来存放各个项的索引
 
 treeBuilder = tree_builder.Tree_builder(routines=routines, min_sup=min_sup, headerTable=headerTable)    #建造FP_Tree
