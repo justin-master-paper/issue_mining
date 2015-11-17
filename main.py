fdaea653be61881931d42d9f410cd7bb45386429
Justin Leo Ye <justinleoye@gmail.com>
2015-11-17 11:52:43 +0800

    fix min_sup

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fp_growth import tree_builder, tree_miner
 from issue_routines import get_issue_routines
 
 routines = get_issue_routines()
-min_sup = 3                             #最小支持度计数
+min_sup = int(len(routines) * 0.1)                             #最小支持度计数
 headerTable = {}        #头结点表，用来存放各个项的索引
 
 treeBuilder = tree_builder.Tree_builder(routines=routines, min_sup=min_sup, headerTable=headerTable)    #建造FP_Tree
